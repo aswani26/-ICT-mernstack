e40832179ef3cdd21df7f8415cf6e6a3b0ca80b6
aswani26 <aswania9365@gmail.com>
2026-02-16 16:38:48 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -2,42 +2,40 @@
 const express=require("express")
 require("./connectin")
 //initialize
-const app = express()
-app.use(express.json())
 var stumodel=require("./model/student")
-const { deleteModel } = require("mongoose")
-//api creation
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+var cors=require("cors")
 
-app.get('/sample', (req, res) => {
-  res.send('hello ')
-})
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+//api creation
+
 
 //add
-app. post('/add',(req,res)=>{
-    stumodel(req.body).save()
+app.post('/add',async(req,res)=>{
+     await stumodel(req.body).save()
     res.send("data added")
 })
-//view
-app. get
-('/view',async(req,res)=>{
+//vie
+app.get('/view',async(req,res)=>{
    var data= await stumodel.find()
     res.send(data)
 })
 //delte 
 app.delete('/delete/:id',async(req,res)=>{
-    await stumodel. findByIdAndDelete(req.params.id)
+    await stumodel.findByIdAndDelete(req.params.id)
     res.send("data delete")
     
 
 })
 app.put('/update/:id',async(req,res)=>{
-    await stumodel. findByIdAndUpdate(req.params.id,req.body)
+    await stumodel.findByIdAndUpdate(req.params.id,req.body)
     res.send("data update")
     
 })
+
     
 
 
